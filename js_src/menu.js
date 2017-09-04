68c1ae91ee4edea58d951cda8ed2f6aaeacc809b
yoosoohyun <yoosu59@gmail.com>
2017-09-04 18:28:36 +0900

    2017-09-04

--- js_src/menu.js
+++ js_src/menu.js
@@ -2,39 +2,34 @@
  * Created by Administrator on 2017-08-24.
  */
 
-//$(function(){
-//
-//  $('.menu-title-link').on('mouseenter', function(){
-//    $('.menu-landmark').removeClass('on');
-//
-//    $(this).on('mouseenter', function(){
-//      $('.menu-schedule').addClass('on');
-//    });
-//
-//  });
-//
-//
-//  $('.menu-title-item').on('mouseenter', function(){
-//    $('.menu-schedule').removeClass('on');
-//
-//    $(this).on('mouseenter', function(){
-//      $('.menu-landmark').addClass('on');
-//    });
-//
-//  });
-//
-//});
 
 
+$(function(){
+  $('.menu-title-item').on('mouseenter', function(){
+    $('.menu-schedule').addClass('on');
+    $('.menu-landmark').removeClass('on');
+  });
 
-//
+  $('.menu-title-item').eq(0).on('mouseenter', function(){
+    $('.menu-schedule').removeClass('on');
+    $('.menu-landmark').addClass('on');
+  });
+
+  $('.menu-title-item').eq(2).on('mouseenter', function(){
+    $('.menu-schedule').removeClass('on');
+    $('.menu-landmark').removeClass('on');
+  });
+
+});
+
+
 //$(function(){
 //  $('.menu-title-item').on('mouseenter', function(){
 //    $('.menu-schedule').addClass('on');
 //    $('.menu-landmark').removeClass('on');
 //  });
 //
-//  $('.menu-title-item').eq(0).on('mouseenter', function(){
+//  $('.menu-title-item').eq(1).on('mouseenter', function(){
 //    $('.menu-schedule').removeClass('on');
 //    $('.menu-landmark').addClass('on');
 //  });
@@ -44,24 +39,4 @@
 //    $('.menu-landmark').removeClass('on');
 //  });
 //
-//
-//});
-
-
-//$(function(){
-//  $('.menu-title-item').on('mouseenter', function(){
-//
-//    //$('menu-schedule').removeClass('on');
-//
-//    $('.menu-landmark').addClass('on');
-//  });
-//
-//  $('.menu-title-item').on('mouseleave', function(){
-//    $('.menu-landmark').removeClass('on');
-//  });
-//
-//  $('.menu-title-item').children().on('mouseenter', function(){
-//    $('.menu-landmark').removeClass('on');
-//    $('.menu-schedule').addClass('on');
-//  });
 //});
